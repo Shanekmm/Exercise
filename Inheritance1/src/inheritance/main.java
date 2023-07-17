e023cee619f75e152126c3b6f43afc23ec643b96
Shanekmm <124464343+Shanekmm@users.noreply.github.com>
2023-07-17 22:48:01 +0530

    Update main.java

--- Inheritance1/src/inheritance/main.java
+++ Inheritance1/src/inheritance/main.java
@@ -7,15 +7,15 @@ public class main {
 		// TODO Auto-generated method stub
 		Bike splendor=new Bike();//overloading
 		Car audi=new Car();
-		//splendor.move();
-    	//splendor.displayBike();
-//		System.out.println(splendor.speed);
-		//audi.stop();
-//		System.out.println(splendor.wheels);
-//		System.out.println(audi.wheels);
-//		audi.print();//overriding
-    	//splendor.print();//overriding
-//		audi.Park();//final
+		splendor.move();
+    	splendor.displayBike();
+		System.out.println(splendor.speed);
+		audi.stop();
+		System.out.println(splendor.wheels);
+		System.out.println(audi.wheels);
+		audi.print();//overriding
+    	splendor.print();//overriding
+		audi.Park();//final
 		Vehicle v=new Car();//upcasting
 		v.print();
 		if(v instanceof Car) {
